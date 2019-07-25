f1640ac15482b0ac9d70f370e14281298f4e6008
Hasan KARAOĞLU <hkaraoglutr@gmail.com>
2019-07-26 00:28:51 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,166 @@
+class QueryBuilder
+{
+  constructor(table, dbInstance)
+  {
+    this.isFirstWhere = true;
+    this.table = table;
+    this.db = dbInstance;
+    this.sql = "";
+    this.values = [];
+  }
+  select(columns)
+  {
+    if(columns === undefined)
+    {
+      columns = ["*"];
+    }
+    this.sql = [this.sql, "SELECT", columns.join(", "), "FROM", this.table].join(" ");
+    return this;
+  }
+
+  insert(data)
+  {
+    let parameters = [];
+    Object.keys(data).forEach(function (item)
+    {
+      parameters.push([item, "=", "?"].join(" "));
+    });
+    this.table = this.table.split(" ")[0];
+    this.values = Object.values(data);
+    this.sql = [this.sql, "INSERT INTO", this.table, "SET",  parameters.join(", ")].join(" ");
+    return this.get();
+  }
+
+  update(data)
+  {
+    let parameters = [];
+    Object.keys(data).forEach(function (item)
+    {
+      parameters.push([item, "=", "?"].join(" "));
+    });
+    this.values = Object.values(data);
+    this.sql = ["UPDATE", this.table, "SET",  parameters.join(", "), this.sql].join(" ");
+    return this.get();
+  }
+
+  delete()
+  {
+    this.table = this.table.split(" ")[0];
+    this.sql = ["DELETE", "FROM", this.table, this.sql].join(" ");
+    return this.get();
+  }
+
+  join(table, values)
+  {
+    return this.joinCommon(table, values);
+  }
+
+  leftJoin(table, values)
+  {
+    return this.joinCommon(table, values);
+  }
+
+  rightJoin(table, values)
+  {
+    return this.joinCommon(table, values);
+  }
+
+  joinCommon(table, values)
+  {
+    let onValues =[];
+    if(!Array.isArray(values))
+    {
+      onValues.push([values.first, values.operator, values.second].join(" "));
+    }
+    else
+    {
+      values.forEach(function (item) {
+        onValues.push([item.first, item.operator, item.second].join(" "));
+      });
+    }
+    this.sql = [this.sql, "INNER JOIN", table, "ON", onValues.join(" AND ")].join(" ");
+    return this;
+  }
+
+  whereCommon(first, operator, second, type)
+  {
+    if(this.isFirstWhere)
+    {
+      this.isFirstWhere = false;
+      this.sql = [this.sql, "WHERE", first, operator, second].join(" ");
+    }
+    else
+    {
+      this.sql = [this.sql, type, first, operator, second].join(" ");
+    }
+    return this;
+  };
+
+  where(first, operator, second)
+  {
+    return this.whereCommon(first, operator, second, 'AND');
+  }
+
+  whereOr(first, operator, second)
+  {
+    return this.whereCommon(first, operator, second, 'OR')
+  }
+
+  whereIn(column, values)
+  {
+    if(this.isFirstWhere)
+    {
+      this.isFirstWhere = false;
+      this.sql = [this.sql, "WHERE", column, "IN(", values.join(", "), ")"].join(" ");
+    }
+    else
+    {
+      this.sql = [this.sql, "AND", column, "IN(", values.join(", "), ")"].join(" ");
+    }
+    return this;
+  }
+
+  orderBy(column, order)
+  {
+    this.sql = [this.sql, "ORDER BY", column, order].join(" ");
+    return this;
+  }
+
+  groupBy(columns)
+  {
+    if(!Array.isArray(columns))
+    {
+      columns = [columns];
+    }
+    this.sql = [this.sql, "GROUP BY", columns.join(", ")].join(" ");
+    return this;
+  }
+
+  having(first, operator, second)
+  {
+    this.sql = [this.sql, "HAVING", first, operator, second].join(" ");
+    return this;
+  }
+
+  limit(start, length)
+  {
+    if(length === undefined)
+    {
+      this.sql = [this.sql, "LIMIT", start].join(" ");
+    }
+    else
+    {
+      this.sql = [this.sql, "LIMIT", start, ",", length].join(" ");
+    }
+    return this;
+  }
+
+  async get()
+  {
+    let result = await this.db.execute(this.sql, this.values);
+    this.sql = "";
+    return {err: "", results: []};
+  }
+
+}
+module.exports = QueryBuilder;
---
+++ readme.md
@@ -0,0 +1,107 @@
+
+NodeJS Express framework mysql query builder and executor library. Inspired by PHP Laravel Framework.
+
+
+**Initialize:**
+
+    var db = new QueryBuilder(table, mysqlConnection));
+    // mysqlConnection: Your mysql connection instance 
+    // your primary table name to use after from
+	
+	
+**SELECT:**		
+   
+   If you call select function **without arguments,** it selects **all columns** as default.
+
+    var db = new QueryBuilder("address AS a", mysqlConnection));
+    db.select()
+      .where("address_id", "=", 1)
+      .where("customer_id", "=", 134)
+      .get()
+    
+ If you want to select **specific** columns then:
+
+	db.select(["address_id", "firstname", "lastname", "zipCode"])
+      .where("address_id", "=", 1)
+      .where("customer_id", "=", 134)
+      .get()
+
+   **UPDATE:**
+	   
+If you want to update columns on a table then:
+
+     db.where("customer_id", "=", 134)
+       .update({"firstname" : "Hasan", "lastname" : "Karaoğlu"});
+
+
+**INSERT:**
+If you want to insert a record into a table then:
+
+    db.insert({
+	    "firstname"   : "Hasan", 
+	    "lastname"    : "Karaoğlu",
+	    "address_id"  : 2,
+	    "customer_id" : 134 
+    })
+
+**DELETE:**
+If you want to delete a record from a table then:
+
+    db.where("customer_id", "=", 134)
+      .where("address_id", "=", 1)
+      .delete()
+
+**JOIN:**
+
+If you want to join a table, then:
+	
+
+    db.select()
+      .join("customer AS c", { first : "c.customer_id", operator: "=", "second" : "a.customer_id"})
+      .where("a.customer_id", "=", 134)
+      .get()
+
+You can also use **leftJoin** and **rightJoin**	 functions to use other join types.
+
+**ORDER BY:**
+If you want to order results, then:
+
+    db.select()
+      .orderBy("firstname", "desc")
+      .get()
+
+**GROUP BY:**
+  If you want to group results, then:
+
+      db.select("COUNT(address_id)")
+        .groupBy("customer_id")
+        .get() 
+or for multiple grouping:
+		
+	  db.select("COUNT(address_id)")
+        .groupBy(["customer_id", "zipCode"])
+        .get() 
+
+**HAVING:**
+If you want to filter group results, then:
+	
+
+    db.select("COUNT(address_id)")
+      .groupBy("customer_id") 
+      .having("zipCode", "<",  34000)
+      .get()
+
+**LIMIT:**
+If you want to limit records count then:
+
+     db.select()
+	   .limit(100)
+	   .get()
+or
+
+    db.select()
+      .limit(0, 100)
+      .get()
+
+ 
+
